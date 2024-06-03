122f0b690f4ee22343bfc4a1b301ce6720ce616e
RISAL <149997333+risalrmlng4@users.noreply.github.com>
2024-06-03 11:38:35 +0800

    TUGAS 2

---
+++ 123.js
@@ -0,0 +1,5 @@
+function myFunction() {
+	document.getElementById("1").innerHTML = "RISAL";
+	document.getElementById("2").innerHTML = "105841111023";
+	document.getElementById("3").innerHTML = "2.C IF";
+}
